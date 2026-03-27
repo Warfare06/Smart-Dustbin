3a66d41b66c18b05a5948f2fa1a1ad3484773c92
Warfare06 <warfareyt2@gmail.com>
2026-03-27 22:01:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBowxOjZiwCQdBIsmPigIdKRGjY6ObVtKQ",
   authDomain: "smart-dustbin-b43d8.firebaseapp.com",
   databaseURL: "https://smart-dustbin-b43d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smart-dustbin-b43d8"
+  projectId: "smart-dustbin-b43d8",
+  storageBucket: "smart-dustbin-b43d8.firebasestorage.app",
+  messagingSenderId: "795207450873",
+  appId: "1:795207450873:web:4f9f6a11f6fced31902228",
+  measurementId: "G-5XSBSXF245"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -16,12 +20,14 @@ function selectBin(bin) {
     db.ref("smart_dustbin/" + bin).on("value", function(snapshot) {
         const data = snapshot.val();
 
-        document.getElementById("level").innerText = data.level;
-        document.getElementById("waste_type").innerText = data.waste_type;
-        document.getElementById("status").innerText = data.status;
-        document.getElementById("moisture").innerText = data.moisture_value;
+        if(data){
+            document.getElementById("level").innerText = data.level;
+            document.getElementById("waste_type").innerText = data.waste_type;
+            document.getElementById("status").innerText = data.status;
+            document.getElementById("moisture").innerText = data.moisture_value;
 
-        document.getElementById("fillLevel").style.height = data.level + "%";
+            document.getElementById("fillLevel").style.height = data.level + "%";
+        }
     });
 }
 
