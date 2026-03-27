3f57e0d120daa5dea7ba8cb0cf5d17336de0e41e
Warfare06 <warfareyt2@gmail.com>
2026-03-27 21:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBowxOjZiwCQdBIsmPigIdKRGjY6ObVtKQ",
-    authDomain: "smart-dustbin-b43d8.firebaseapp.com",
-    databaseURL: "https://smart-dustbin-b43d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "smart-dustbin-b43d8",
-    storageBucket: "smart-dustbin-b43d8.firebasestorage.app",
-    messagingSenderId: "795207450873",
-    appId: "1:795207450873:web:4f9f6a11f6fced31902228",
-    measurementId: "G-5XSBSXF245"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "smart-dustbin-b43d8.firebaseapp.com",
+  databaseURL: "https://smart-dustbin-b43d8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-dustbin-b43d8"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-db.ref("smart_dustbin/bin1").on("value", function(snapshot) {
-    const data = snapshot.val();
+let currentBin = "bin1";
 
-    document.getElementById("level").innerText = data.level + "%";
-    document.getElementById("waste_type").innerText = data.waste_type;
-    document.getElementById("status").innerText = data.status;
-    document.getElementById("lid_status").innerText = data.lid_status;
-});
+function selectBin(bin) {
+    currentBin = bin;
+
+    db.ref("smart_dustbin/" + bin).on("value", function(snapshot) {
+        const data = snapshot.val();
+
+        document.getElementById("level").innerText = data.level;
+        document.getElementById("waste_type").innerText = data.waste_type;
+        document.getElementById("status").innerText = data.status;
+        document.getElementById("moisture").innerText = data.moisture_value;
+
+        document.getElementById("fillLevel").style.height = data.level + "%";
+    });
+}
 
 function openLid() {
-    db.ref("smart_dustbin/bin1/lid_command").set("open");
+    db.ref("smart_dustbin/" + currentBin + "/lid_command").set("open");
 }
+
+function sendAlert() {
+    db.ref("alerts/" + currentBin).set("FULL_BIN_ALERT");
+}
+
+selectBin("bin1");
