5297a561b934b75536485d35defc3dce90cc4e46
Warfare06 <warfareyt2@gmail.com>
2026-03-27 23:18:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,17 @@ function selectStreet(street) {
     loadStreetData();
 }
 
+function setBinFill(fillId, level) {
+    document.getElementById(fillId).style.height = level + "%";
+
+    if (level < 30)
+        document.getElementById(fillId).style.background = "green";
+    else if (level < 70)
+        document.getElementById(fillId).style.background = "yellow";
+    else
+        document.getElementById(fillId).style.background = "red";
+}
+
 function loadStreetData() {
     db.ref("smart_city/" + currentStreet)
       .on("value", function(snapshot) {
@@ -36,18 +47,22 @@ function loadStreetData() {
         document.getElementById("level1").innerText = data.bin1.level;
         document.getElementById("type1").innerText = data.bin1.waste_type;
         document.getElementById("status1").innerText = data.bin1.status;
+        setBinFill("fill1", data.bin1.level);
 
         document.getElementById("level2").innerText = data.bin2.level;
         document.getElementById("type2").innerText = data.bin2.waste_type;
         document.getElementById("status2").innerText = data.bin2.status;
+        setBinFill("fill2", data.bin2.level);
 
         document.getElementById("level3").innerText = data.bin3.level;
         document.getElementById("type3").innerText = data.bin3.waste_type;
         document.getElementById("status3").innerText = data.bin3.status;
+        setBinFill("fill3", data.bin3.level);
 
         document.getElementById("level4").innerText = data.bin4.level;
         document.getElementById("type4").innerText = data.bin4.waste_type;
         document.getElementById("status4").innerText = data.bin4.status;
+        setBinFill("fill4", data.bin4.level);
     });
 }
 
