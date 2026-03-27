95ce1d7fe7e1469b5ae83bdea26d7a6f8ec12766
Warfare06 <warfareyt2@gmail.com>
2026-03-27 23:03:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBowxOjZiwCQdBIsmPigIdKRGjY6ObVtKQ",
   authDomain: "smart-dustbin-b43d8.firebaseapp.com",
   databaseURL: "https://smart-dustbin-b43d8-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "smart-dustbin-b43d8",
   storageBucket: "smart-dustbin-b43d8.firebasestorage.app",
   messagingSenderId: "795207450873",
-  appId: "1:795207450873:web:4f9f60a11f6fced31902228"
+  appId: "1:795207450873:web:4f9f6a11f6fced31902228",
+  measurementId: "G-5XSBSXF245"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-// Current street and bin
 let currentStreet = "street1";
 let currentBin = "bin1";
 
-// Select Street
 function selectStreet(street) {
     currentStreet = street;
+    document.getElementById("streetTitle").innerText = street.toUpperCase() + " Bins";
+    loadStreetBins();
     loadData();
 }
 
-// Select Bin
 function selectBin(bin) {
     currentBin = bin;
     loadData();
 }
 
-// Load Data from Firebase
+function loadStreetBins() {
+    const bins = document.getElementsByClassName("small-bin");
+
+    db.ref("smart_city/" + currentStreet).once("value", function(snapshot) {
+        const streetData = snapshot.val();
+        let i = 0;
+
+        for (let bin in streetData) {
+            let level = streetData[bin].level;
+
+            if (level < 30) bins[i].style.background = "green";
+            else if (level < 70) bins[i].style.background = "yellow";
+            else bins[i].style.background = "red";
+
+            i++;
+        }
+    });
+}
+
 function loadData() {
     db.ref("smart_city/" + currentStreet + "/" + currentBin)
       .on("value", function(snapshot) {
@@ -42,10 +58,8 @@ function loadData() {
             document.getElementById("status").innerText = data.status;
             document.getElementById("moisture").innerText = data.moisture_value;
 
-            // Fill bin animation
             document.getElementById("fillLevel").style.height = data.level + "%";
 
-            // Lid status animation
             if (data.lid_status === "open") {
                 document.getElementById("lid").style.transform = "rotate(-60deg)";
             } else {
@@ -55,23 +69,20 @@ function loadData() {
     });
 }
 
-// Open Lid
 function openBin() {
     document.getElementById("lid").style.transform = "rotate(-60deg)";
     db.ref("smart_city/" + currentStreet + "/" + currentBin + "/lid_command").set("open");
 }
 
-// Close Lid
 function closeBin() {
     document.getElementById("lid").style.transform = "rotate(0deg)";
     db.ref("smart_city/" + currentStreet + "/" + currentBin + "/lid_command").set("close");
 }
 
-// Send Alert
 function sendAlert() {
     db.ref("alerts/" + currentStreet + "_" + currentBin).set("FULL_BIN_ALERT");
-    alert("Alert sent for " + currentStreet + " " + currentBin);
+    alert("Alert sent!");
 }
 
-// Load default data on start
+loadStreetBins();
 loadData();
